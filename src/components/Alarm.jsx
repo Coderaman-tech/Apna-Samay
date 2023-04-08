98640126752dc36bee86dd2b6d733034bfaa816e
coderaman_tech <amankumarshrivastava835@gmail.com>
2023-04-08 20:47:32 +0530

    update

--- src/components/Alarm.jsx
+++ src/components/Alarm.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { GrAdd } from 'react-icons/gr';
 
 function Alarm() {
   return (
     <div>
       <h1 className='text-5xl text-sky-600 '>Alarm</h1>
+      <h3 className=''> Lets go for a
+
+        <GrAdd Close
+          className="icon"
+          size="50px"
+          color="white" />
+
+      </h3>
     </div>
   )
 }
