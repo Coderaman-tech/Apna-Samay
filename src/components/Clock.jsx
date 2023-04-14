fe406209685a63ac9c73a8ac7f1de866f6b8d0b5
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2023-04-15 02:51:26 +0530

    final commit

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -36,7 +36,7 @@ function Clock() {
       
     <div className='text-center object-right' >
       <h1 className='text-sky-600 text-5xl c-slate-500'>Digital Clock</h1>
-      <h2 className='text-4xl m-9 dark:text-white'>{hour}:{minute}:{second} {AmOrPm}</h2>
+      <h2 className='text-4xl m-9'>{hour}:{minute}:{second} {AmOrPm}</h2>
     </div>
   </div>
   </>
